5643a19b9acb7aa4cb42aff2a96aace54e230ec9
karamg1998 <111856262+karamg1998@users.noreply.github.com>
2022-09-23 21:26:22 +0530

    small changes

--- task13.js
+++ task13.js
@@ -1,91 +1,123 @@
-//var my_name=document.getElementById('name');
-//var age=document.getElementById('age');
-
 var submit=document.getElementById('submit');
 
-submit.addEventListener('click',run);
+submit.addEventListener('click',Run);
 
-
-
-function run(e)
-{
+function Run(e){
     e.preventDefault();
-    var main=document.querySelector('main');
-    var new_element=document.createElement('div');
-    new_element.id='div';
+    var name=document.getElementById('name').value;
+    var email=document.getElementById('email').value;
+    var phone=document.getElementById('pnumber').value;
     
-    var text1=document.createTextNode("=>Name:"+document.getElementById('name').value+" ");
-    new_element.appendChild(text1);
+    var myobj={
+        Name:name,
+        Email:email,
+        Phone:phone
+       } 
+      
+    user(myobj);
     
-
-    var text2=document.createTextNode("Email:"+document.getElementById('email').value+" ");
-    new_element.appendChild(text2);
-
-    var text3=document.createTextNode("Phone:"+document.getElementById('pnumber').value);
-    new_element.appendChild(text3);
-
-   var button=document.createElement('button');
-   button.className="btn btn-danger btn-sm float-right delete";
-   button.textContent="Delete";
-
-   new_element.appendChild(button);
-
-   var button2=document.createElement('button');
-   button2.className="btn btn-dark btn-sm float-right edit";
-   button2.textContent="Edit";
-
-   new_element.appendChild(button2);
-   
-    /* var insert=document.querySelector('body .container');
-    var insert1=document.querySelector('body main')
-
-   insert.insertBefore(new_element,insert1); */
-   
-   main.appendChild(new_element);
-  
-   var myobj={
-    Name:document.getElementById('name').value,
-    Email:document.getElementById('email').value,
-    Phone:document.getElementById('pnumber').value
-   } 
-   var obj_serialised=JSON.stringify(myobj);
-  
-  
-   localStorage.setItem(document.getElementById('email').value,obj_serialised);
-   
+     
 }
 
-//to add delete functionality
-var del=document.querySelector('.container');
 
-del.addEventListener('click',RemoveItem); 
-del.addEventListener('click',editItem);
+    function user(data)
+    {
 
-function RemoveItem(e)
-{
-    for(var i=0;i<localStorage.length;i++)
-    {
-        var key=localStorage.key(i);
-        if(e.target.classList.contains('delete'))
-    {
+        var obj_serialised=JSON.stringify(data);
+        
+        localStorage.setItem(data.Email, obj_serialised);
+
+        var main=document.querySelector('.container');
+    
+        var new_element=document.createElement('li');
+        
+        new_element.id=data.Email;
        
-            var li=e.target.parentElement;
-            del.removeChild(li);
-            localStorage.removeItem(key);
-    }
-    }
+        var data1=document.createTextNode("=>Expense:"+data.Name+"-");
+        new_element.appendChild(data1);
     
+        var data2=document.createTextNode("Description:"+data.Email+"-");
+        new_element.appendChild(data2);
+    
+        var data3=document.createTextNode("Category:"+data.Phone+" ");
+        new_element.appendChild(data3);
+    
+    
+        var button_e=document.createElement('button');
+        button_e.className="edit";
+        button_e.textContent="Edit";
+        new_element.appendChild(button_e);
+    
+        var button_d=document.createElement('button');
+        button_d.className="delete";
+        button_d.textContent="Delete";
+        new_element.appendChild(button_d);
+        
+        main.appendChild(new_element)
+    }
 
+   
+    
+var delete_expense=document.querySelector('.container');
+delete_expense.addEventListener('click',run2);
+
+
+
+function run2(e)
+{
+    if(e.target.classList.contains('delete'))
+    {
+        var li=e.target.parentElement;
+        delete_expense.removeChild(li);
+        var id=li.id;
+        
+        for(var i=0;i<localStorage.length;i++)
+      {
+        var s=localStorage.key(i);
+        if(s===id)
+        {
+            localStorage.removeItem(s);
+            
+        }
+        
+      }
+        
+    }
 }
 
-function editItem(e)
+var edit=document.querySelector('.container');
+edit.addEventListener('click',run3);
+ 
+function run3(e)
 {
+
     if(e.target.classList.contains('edit'))
     {
-       
-            var li=e.target.parentElement;
-            del.removeChild(li);
-            
+        var li1=e.target.parentElement;
+        edit.removeChild(li1);
+        var object=JSON.parse(localStorage.getItem(li1.id));
+        retrive(object);
+        
+        for(var i=0;i<localStorage.length;i++)
+        {
+          var s=localStorage.key(i);
+          if(s===li1.id)
+          {
+              localStorage.removeItem(s);
+              
+          }
+          
+        }
+        
     }
+}
 
+function retrive(user)
+{
+    var e=document.getElementById('name');
+    e.value=user.Name;
+    var d=document.getElementById('email');
+    d.value=user.Email;
+    var c=document.getElementById('pnumber');
+    c.value=user.Phone;
 }
